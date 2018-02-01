a2e79ee5121cf5e56164736212be6c9ad647d80a
sv18 <502718948@logon.ds.ge.com>
2018-02-01 11:42:48 -0800

    hello web server

---
+++ webserver/hellowebserver.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil{
+		fmt.Println("error with listen and serve:", err)
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello!"))
+	//fmt.Fprintln(w, "Hello!") // alternative way
+}
